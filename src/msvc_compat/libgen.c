1f51c6e7ce39b899f88361f7939a1f9324c0291b
Alberto Fustinoni <alberto@aftnet.net>
2022-09-20 18:01:11 +0900

    Fix

--- src/msvc_compat/libgen.c
+++ src/msvc_compat/libgen.c
@@ -1,6 +1,12 @@
 #include "libgen.h"
 
+#include <stdlib.h>
+#include <string.h>
+
 char *basename(char *path)
 {
-    return 0;
+    static char filename[_MAX_FNAME + _MAX_EXT];
+    static char ext[_MAX_EXT];
+    _splitpath_s(path, NULL, 0, NULL, 0, filename, _MAX_FNAME, ext, _MAX_EXT);
+    return strcat(filename, ext);
 }
